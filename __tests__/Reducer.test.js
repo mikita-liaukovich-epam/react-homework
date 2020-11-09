05b01cb4775508bff6654be1c37565e29607dda7
Mikita Liaukovich <nicklevkovic@gmail.com>
2020-11-09 22:23:59 +0300

    feat: add coverage test

--- __tests__/Reducer.test.js
+++ __tests__/Reducer.test.js
@@ -10,9 +10,35 @@ const emptyState = {
   selectedSort: 0,
 }
 
+const mockData = [
+  {release_date: "2019-02-07", vote_average: 7.2},
+  {release_date: "2018-01-06", vote_average: 6.1},
+  {release_date: "2020-03-08", vote_average: 8.3},
+]
+
 describe("Root.reducer", () => {
   it('returns empty state if no states were passed', () => {
     const res = JSON.stringify(reducer());
     expect(res).toEqual(JSON.stringify(emptyState));
+  });
+
+  it('returns updated state if setState was called', () => {
+    const res = JSON.stringify(reducer(undefined, {
+      type: 'setState',
+      payload: {
+        modal: 'addition'
+      }
+    }));
+    expect(res).toEqual(JSON.stringify(Object.assign(emptyState, { modal: "addition" })));
+  });
+
+  it('returns sorted data if setState was called', () => {
+    const res = JSON.stringify(reducer(Object.assign(emptyState, {data: mockData}), {
+      type: 'sortDataBy',
+      payload: 'rating'
+    }));
+    expect(res).toEqual(JSON.stringify(Object.assign(emptyState, { 
+      data: mockData.sort((a, b) => b.vote_average - a.vote_average)
+    })));
   });
 })
--- __tests__/SearchForm.test.js
+++ __tests__/SearchForm.test.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import SearchForm from '../src/components/SearchForm/SearchForm'
 import renderer from 'react-test-renderer'
+import { Provider } from 'react-redux'
+import store from '../src/redux/store'
 
 describe("SearchForm", () => {
   it('renders correctly', () => {
     const tree = renderer
-      .create(<SearchForm />)
+      .create(<Provider store={store}><SearchForm /></Provider>)
       .toJSON();
     expect(tree).toMatchSnapshot();
   });
