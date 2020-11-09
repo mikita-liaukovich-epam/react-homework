b5f20e8501bef49b087e86e0f903c30774be0bb9
Mikita Liaukovich <nicklevkovic@gmail.com>
2020-11-09 22:23:59 +0300

    feat: add coverage test

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,16 @@
 module.exports = {
   moduleNameMapper: {
     "^.+\\.(css|scss)$": "babel-jest",
-  }
+  },
+  collectCoverage: true,
+  coverageReporters: ["lcov"],
+  coverageDirectory: "test-coverage",
+  coverageThreshold: {
+    global: {
+      branches: 0,
+      functions: 0,
+      lines: 0,
+      statements: 0
+    }
+  },
 };
